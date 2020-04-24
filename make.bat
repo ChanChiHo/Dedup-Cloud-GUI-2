2a2d5f5fe1d20b57878a7ebd11f6d37ec44bf208
ChanChiHo <32035995+ChanChiHo@users.noreply.github.com>
2020-04-24 18:45:48 +0800

    Version 1.2.2 Window Script Updated

--- make.bat
+++ make.bat
@@ -1,8 +1,8 @@
-keytool -genkey -dname "cn=PCL1904, ou=CSE, o=CUHK, c=HK" -keystore sslclientkeys -storetype PKCS12 -keyalg RSA -storepass "password"
-keytool -export -keystore sslclientkeys -file sslclient.cer -storepass "password"
+keytool -genkey -alias sslclient1 -dname "cn=PCL1904, ou=CSE, o=CUHK, c=HK" -keystore sslclientkeys -storetype PKCS12 -keyalg RSA -storepass "password"
+keytool -export -alias sslclient1 -keystore sslclientkeys -file sslclient.cer -storepass "password"
 keytool -genkey -alias sslserver -dname "cn=PCL1904, ou=CSE, o=CUHK, c=HK" -keystore sslserverkeys -storetype PKCS12 -keyalg RSA -storepass "password"
 keytool -export -alias sslserver -keystore sslserverkeys -file sslserver.cer -storepass "password"
-keytool -import -keystore sslservertrust -file sslclient.cer -storepass "password" -noprompt
+keytool -import -alias sslclient1 -keystore sslservertrust -file sslclient.cer -storepass "password" -noprompt
 keytool -import -alias sslserver -keystore sslclienttrust -file sslserver.cer -storepass "password" -noprompt
 
 move sslclient* Client
